98c080c2b0cf8212bcdb33d383079dff41608189
pavlogrechka <pavlogrechka@gmail.com>
2022-02-21 19:48:46 +0200

    add axios fecth data from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { usePosts } from './hooks/usePosts';
 import PostsList from './components/PostsList';
 import './styles/App.css';
 import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
+import MyModal from './UI/MyModal/MyModal';
+import MyButton from './UI/buttons/MyButton';
+import axios from 'axios';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'aaa', body: '111eeee' },
-    { id: 2, title: '333bbb 2', body: '3333ffff' },
-    { id: 3, title: 'ccc 3', body: '222gggg' },
-    { id: 4, title: 'dddd 3', body: '8888gggg' },
-    { id: 5, title: 'eeee 3', body: '01010gggg' },
-    { id: 6, title: '9999ffff 3', body: '8888gggg' },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({
     sort: '',
     query: '',
   });
 
-  const sortedPosts = useMemo(() => {
-    console.log('sort function');
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
+  const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query)
-    );
-  }, [filter.query, sortedPosts]);
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false)
   };
+
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    setPosts(response.data)
+  }
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -45,7 +39,14 @@ function App() {
 
   return (
     <div className='App'>
-      <PostForm create={createPost} />
+      <button onClick={fetchPosts}>get posts</button>
+      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+        Create User
+      </MyButton>
+
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
 
       <hr style={{ margin: '15px 0' }} />
 
@@ -61,5 +62,3 @@ function App() {
 }
 
 export default App;
-
-// https://youtu.be/GNrdg3PzpJQ?t=5037
